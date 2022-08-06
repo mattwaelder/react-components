89d45b5888a0b2aa5b7ffefe02bd9a47417c97bc
Matt Waelder <mattwaelder@gmail.com>
2022-08-06 15:41:30 -0700

    new skills, new react grocery list!

--- app.js
+++ app.js
@@ -1,1 +1,57 @@
-// TODO
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.onListItemClick = this.onListItemClick.bind(this);
+    this.state = {list: ['cucumber', 'butter', 'romaine', 'onions']};
+  }
+
+
+  onListItemClick (e) {
+    alert(`${e.target.innerHTML} selected`);
+  }
+
+  render() {
+    return(
+    <div>
+      <h1>My Grocery List</h1>
+      <GroceryList items = {this.state.list}
+        onListItemClick = {(e) => {this.onListItemClick(e)}}
+      />
+    </div>
+    )};
+}
+
+/////////////////////////////////////////////////////
+
+var GroceryList = (props) => {
+
+  let groceries = [];
+
+  props.items.map((item) => {
+    groceries.push(
+
+        <GroceryListItem
+        name = {item}
+        onListItemClick = {(e) => props.onListItemClick(e)}
+        item = {item} />)
+  })
+
+  return(
+    <ul className="list">
+      {groceries}
+    </ul>
+  );
+}
+
+////////////////////////////////////////////////////
+
+var GroceryListItem = (props) => {
+
+  return(
+      <li className="list_item" onClick = {(e) => props.onListItemClick(e)}>{props.item}</li>
+  );
+}
+
+
+ReactDOM.render(<App />, document.getElementById('app'));
